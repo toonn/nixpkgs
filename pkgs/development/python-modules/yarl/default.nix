d200101814630dd483fd19be8cb26217198db789
Frederik Rietdijk <fridh@fridh.nl>
2017-12-30 12:28:47 +0100

    python: yarl: 0.13.0 -> 0.16.0

--- pkgs/development/python-modules/yarl/default.nix
+++ pkgs/development/python-modules/yarl/default.nix
@@ -8,12 +8,12 @@
 
 let
   pname = "yarl";
-  version = "0.13.0";
+  version = "0.16.0";
 in buildPythonPackage rec {
   name = "${pname}-${version}";
   src = fetchurl {
     url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-    sha256 = "25fe681a982f2cec567df8abac7cbd2ac27016e4aec89193945cab0643bfdb42";
+    sha256 = "47622985ecd9b15335d65c1acd54aeb3ba449e6d09b36e37ecfe334c7e7b8d0b";
   };
 
   buildInputs = [ pytest pytestrunner ];
