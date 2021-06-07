c64f74b2c583ed166e805c14e82ce91d309b4202
toonn <toonn@toonn.io>
2021-06-07 21:48:00 +0200

    darwin.xnu: Pass iOS versions to availability.pl
    On macOS 10.13.6 the iOS versions in
    `/usr/include/sys/_symbol_aliasing.h` go to 11.3.

--- pkgs/os-specific/darwin/apple-source-releases/xnu/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/xnu/default.nix
@@ -75,7 +75,7 @@ appleDerivation' (if headersOnly then stdenvNoCC else stdenv) ({
       if [ "\$1" == "--macosx" ]; then
         echo 10.0 10.1 10.2 10.3 10.4 10.5 10.6 10.7 10.8 10.9 10.10 10.11 10.12 10.13
       elif [ "\$1" == "--ios" ]; then
-        echo 2.0 2.1 2.2 3.0 3.1 3.2 4.0 4.1 4.2 4.3 5.0 5.1 6.0 6.1 7.0 8.0 9.0
+        echo 2.0 2.1 2.2 3.0 3.1 3.2 4.0 4.1 4.2 4.3 5.0 5.1 6.0 6.1 7.0 8.0 9.0 10.0 11.0
       fi
     EOF
     chmod +x sdk/usr/local/libexec/availability.pl
