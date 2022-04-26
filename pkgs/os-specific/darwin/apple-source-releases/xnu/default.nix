2788017d5bb226c3724be0e34c39dbe549c70ee8
toonn <toonn@toonn.io>
2022-04-26 16:32:28 +0200

    xnu: Back old Availability macros by modern approach
    The old Availability macros approach leads to a combinatorial explosion
    of versions and is annoying to maintain. Apple has switched to a modern
    approach leaning on Clang's `availability` attribute, probably added for
    this exact purpose.
    
    This patch translates the old Availability macros,
    `__OSX_AVAILABLE_STARTING`, `__OSX_AVAILABLE_BUT_DEPRECATED` and
    `__OSX_AVAILABLE_BUT_DEPRECATED_MSG` to invocations of the newer
    `API_AVAILABLE` and `API_DEPRECATED`.

--- pkgs/os-specific/darwin/apple-source-releases/xnu/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/xnu/default.nix
@@ -15,6 +15,7 @@ appleDerivation' (if headersOnly then stdenvNoCC else stdenv) (
   patches = lib.optional stdenv.isx86_64 [
     ./python3.patch
     ./0001-Implement-missing-availability-platform.patch
+    ./0002-Intercept-old-availability-macros-to-use-modern-appr.patch
   ];
 
   postPatch = ''
