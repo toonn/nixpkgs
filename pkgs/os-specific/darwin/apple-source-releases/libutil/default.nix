18db4a68429ef3aa9fd74226d1d7bbfb83b49c23
toonn <toonn@toonn.io>
2022-07-25 21:15:11 +0200

    libutil: Add dev output required for diskdev_cmds

--- pkgs/os-specific/darwin/apple-source-releases/libutil/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libutil/default.nix
@@ -5,16 +5,25 @@ appleDerivation {
 
   nativeBuildInputs = [ xcbuildHook ];
 
+  outputs = [ "out" "dev" ];
+
   xcbuildFlags = [ "-target" "util" ];
 
   installPhase = ''
-    mkdir -p $out/lib
+    mkdir -p $out/lib $out/include
 
     cp Products/Release/*.dylib $out/lib
 
     # TODO: figure out how to get this to be right the first time around
     install_name_tool -id $out/lib/libutil.dylib $out/lib/libutil.dylib
+
+    cp mntopts.h $out/include
   '';
+
+  # FIXME: Header checking doesn't work with multiple outputs 
+  # appleHeaders = ''
+  #   mntopts.h
+  # '';
 
   meta = with lib; {
     maintainers = with maintainers; [ copumpkin ];
